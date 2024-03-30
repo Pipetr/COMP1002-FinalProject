63ec3167424c1cb205dfcf2a5d6ab436937aa6c5
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-03-30 16:17:25 -0400

    Created custom header and footer templates

---
+++ models/global/load_script.js
@@ -0,0 +1,15 @@
+function loadHeaderAndFooter() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(headerHTML => {
+            document.querySelector('header').innerHTML = headerHTML;
+        });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(footerHTML => {
+            document.querySelector('footer').innerHTML = footerHTML;
+        });
+}
+
+loadHeaderAndFooter();
--- models/product/script.js
+++ models/product/script.js
@@ -17,3 +17,20 @@ function slideImage(){
 }
 
 window.addEventListener('resize', slideImage);
+
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    toggle.addEventListener('click', () => {
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+}
+
+showMenu('nav-toggle', 'nav-menu')
