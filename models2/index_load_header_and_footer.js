67c0abb9cf8dc9a7b103ad46fffeefe54046f1c3
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-04-09 21:36:58 -0400

    fixed the issue of the index.html

--- models2/index_load_header_and_footer.js
+++ models2/index_load_header_and_footer.js
@@ -1,13 +1,13 @@
 // This code snippet is solely used to load the header and footer
 
 function loadHeaderAndFooter() {
-    fetch('header.html')
+    fetch('views/header.html')
         .then(response => response.text())
         .then(headerHTML => {
             document.querySelector('header').innerHTML = headerHTML;
         });
 
-    fetch('footer.html')
+    fetch('views/footer.html')
         .then(response => response.text())
         .then(footerHTML => {
             document.querySelector('footer').innerHTML = footerHTML;
